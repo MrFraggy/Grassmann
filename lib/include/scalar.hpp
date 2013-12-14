dc6ee7fb114b618beec49071cd8d7ee275daace4
Laury Landrau <laury.landrau@gmail.com>
2013-12-14 12:51:36 +0100

    Scalaire

--- lib/include/scalar.hpp
+++ lib/include/scalar.hpp
@@ -1,3 +1,23 @@
-namespace gca {
+namespace gca{
+	
+	class GCA_scalar
+	{
+	private:
+		double k;
+
+	public:
+		GCA_scalar();
+		GCA_scalar(const GCA_scalar& other);
+		GCA_scalar(double s);
+		
+		GCA_scalar& operator=(const GCA_scalar& other);
+
+		GCA_scalar& operator^(const GCA_scalar& other);
+		GCA_antiquadvector operator~();
+
+		std::ostream& operator<<(std::ostream& out);
+		GCA_scalar& operator<<(double s);
+
+	};
 	
 }
--- lib/src/scalar.cpp
+++ lib/src/scalar.cpp
@@ -1,3 +1,31 @@
-namespace gca {
+namespace gca{
+
+	GCA_scalar::GCA_scalar() : k(0.0){};
+	GCA_scalar::GCA_scalar(const GCA_scalar& other) : k(other.k){};
+	GCA_scalar::GCA_scalar(double s) : k(s){};
 	
+	GCA_scalar& GCA_scalar::operator=(const GCA_scalar& other){
+		k = other.k;
+		return *this;
+	}
+
+	GCA_scalar& GCA_scalar::operator^(const GCA_scalar& other){
+		k = k*other.k;
+		return *this;
+	}
+
+	/*GCA_antiquadvector GCA_scalar::operator~(){
+		GCA_antiquadvector v();
+		return v;
+	}*/
+
+	std::ostream& GCA_scalar::operator<<(std::ostream& out){
+		out << k << std::endl;
+		return out;
+	}
+	GCA_scalar& GCA_scalar::operator<<(double s){
+		k = s;
+		return *this;
+	}
+
 }
